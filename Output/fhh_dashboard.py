3c69dcb79e70e4123de6855a333e89d649f2cdc5
Kentroth <kentroth+1@gmail.com>
2023-06-16 09:58:20 -0500

    m

--- Output/fhh_dashboard.py
+++ Output/fhh_dashboard.py
@@ -1,17 +1,15 @@
 # Import packages
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
+import numpy as np
 import dash_mantine_components as dmc
+import seaborn as sns
+import openpyxl
 
-# Read Excel files
 future_rev = pd.read_excel('future_rev.xlsx')
 future_occ = pd.read_excel('future_occ.xlsx')
 past_rev = pd.read_excel('past_rev.xlsx')
 past_occ = pd.read_excel('past_occ.xlsx')
-past_adr = pd.read_excel('past_adr.xlsx')
-future_adr = pd.read_excel('future_adr.xlsx')
-past_revpab = pd.read_excel('past_revpab.xlsx')
-future_revpab = pd.read_excel('future_revpab.xlsx')
 
 # Initialize the app - incorporate a Dash Mantine theme
 external_stylesheets = [dmc.theme.DEFAULT_COLORS]
@@ -24,11 +22,7 @@ datasets = {
     'future_rev': future_rev,
     'future_occ': future_occ,
     'past_rev': past_rev,
-    'past_occ': past_occ,
-    'past_adr': past_adr,
-    'future_adr': future_adr,
-    'past_revpab': past_revpab,
-    'future_revpab': future_revpab
+    'past_occ': past_occ
 }
 
 for dataset_name, dataset in datasets.items():
@@ -45,7 +39,6 @@ for dataset_name, dataset in datasets.items():
                 }
             )
 
-            # Add formatting conditions for different ranges
             style.append(
                 {
                     'if': {
@@ -57,7 +50,6 @@ for dataset_name, dataset in datasets.items():
                 }
             )
 
-            # Add formatting conditions for different ranges
             style.append(
                 {
                     'if': {
@@ -69,7 +61,6 @@ for dataset_name, dataset in datasets.items():
                 }
             )
 
-            # Add formatting conditions for different ranges
             style.append(
                 {
                     'if': {
@@ -81,7 +72,6 @@ for dataset_name, dataset in datasets.items():
                 }
             )
 
-            # Add formatting conditions for different ranges
             style.append(
                 {
                     'if': {
@@ -105,7 +95,6 @@ for dataset_name, dataset in datasets.items():
                 }
             )
 
-            # Add formatting conditions for different ranges
             style.append(
                 {
                     'if': {
@@ -117,7 +106,6 @@ for dataset_name, dataset in datasets.items():
                 }
             )
 
-            # Add formatting conditions for different ranges
             style.append(
                 {
                     'if': {
@@ -129,7 +117,6 @@ for dataset_name, dataset in datasets.items():
                 }
             )
 
-            # Add formatting conditions for different ranges
             style.append(
                 {
                     'if': {
@@ -141,7 +128,6 @@ for dataset_name, dataset in datasets.items():
                 }
             )
 
-            # Add formatting conditions for different ranges
             style.append(
                 {
                     'if': {
@@ -152,65 +138,7 @@ for dataset_name, dataset in datasets.items():
                     'color': 'white'
                 }
             )
-        elif dataset_name == 'future_adr' or dataset_name == 'past_adr'or dataset_name == 'past_revpab'or dataset_name == 'past_revpab':
-            style.append(
-                {
-                    'if': {
-                        'column_id': str(col),
-                        'filter_query': '{{{}}} >= 0.01 && {{{}}} <= 10'.format(col, col)
-                    },
-                    'backgroundColor': '#ea5545',
-                    'color': 'white'
-                }
-            )
 
-            # Add formatting conditions for different ranges
-            style.append(
-                {
-                    'if': {
-                        'column_id': str(col),
-                        'filter_query': '{{{}}} >= 10.01 && {{{}}} <= 20'.format(col, col)
-                    },
-                    'backgroundColor': '#ef9b20',
-                    'color': 'white'
-                }
-            )
-
-            # Add formatting conditions for different ranges
-            style.append(
-                {
-                    'if': {
-                        'column_id': str(col),
-                        'filter_query': '{{{}}} >= 20.01 && {{{}}} <= 30'.format(col, col)
-                    },
-                    'backgroundColor': '#ede15b',
-                    'color': 'black'
-                }
-            )
-
-            # Add formatting conditions for different ranges
-            style.append(
-                {
-                    'if': {
-                        'column_id': str(col),
-                        'filter_query': '{{{}}} >= 30.01 && {{{}}} <= 40'.format(col, col)
-                    },
-                    'backgroundColor': '#bdcf32',
-                    'color': 'white'
-                }
-            )
-
-            # Add formatting conditions for different ranges
-            style.append(
-                {
-                    'if': {
-                        'column_id': str(col),
-                        'filter_query': '{{{}}} >= 40.01 && {{{}}} <= 1000'.format(col, col)
-                    },
-                    'backgroundColor': '#87bc45',
-                    'color': 'white'
-                }
-            )
         # Additional style for 0 values
         style.append(
             {
@@ -223,15 +151,10 @@ for dataset_name, dataset in datasets.items():
             }
         )
 
-# Round the datasets to desired decimal places
 future_rev = future_rev.round(0)
 future_occ = future_occ.round(2)
 past_rev = past_rev.round(0)
 past_occ = past_occ.round(2)
-past_adr = past_adr.round(0)
-future_adr = future_adr.round(0)
-past_revpab = past_revpab.round(0)
-future_revpab = future_revpab.round(0)
 
 # App layout
 app.layout = dmc.Container(
@@ -242,8 +165,7 @@ app.layout = dmc.Container(
                 dmc.Col(
                     [
                         dmc.RadioGroup(
-                            [dmc.Radio(i, value=i) for i in ['future_rev', 'future_occ', 'past_rev', 'past_occ',
-                                                             'past_adr', 'future_adr', 'past_revpab', 'future_revpab']],
+                            [dmc.Radio(i, value=i) for i in ['future_rev', 'future_occ', 'past_rev', 'past_occ']],
                             id='my-dmc-radio-item-top',
                             value='future_rev',
                             size="sm",
@@ -265,8 +187,7 @@ app.layout = dmc.Container(
                 dmc.Col(
                     [
                         dmc.RadioGroup(
-                            [dmc.Radio(i, value=i) for i in ['future_rev', 'future_occ', 'past_rev', 'past_occ',
-                                                             'past_adr', 'future_adr', 'past_revpab', 'future_revpab']],
+                            [dmc.Radio(i, value=i) for i in ['future_rev', 'future_occ', 'past_rev', 'past_occ']],
                             id='my-dmc-radio-item-middle',
                             value='future_occ',
                             size="sm",
@@ -288,8 +209,7 @@ app.layout = dmc.Container(
                 dmc.Col(
                     [
                         dmc.RadioGroup(
-                            [dmc.Radio(i, value=i) for i in ['future_rev', 'future_occ', 'past_rev', 'past_occ',
-                                                             'past_adr', 'future_adr', 'past_revpab', 'future_revpab']],
+                            [dmc.Radio(i, value=i) for i in ['future_rev', 'future_occ', 'past_rev', 'past_occ']],
                             id='my-dmc-radio-item-bottom',
                             value='past_rev',
                             size="sm",
@@ -333,31 +253,19 @@ def update_data_table(value_top, value_middle, value_bottom):
         data_top = past_rev.to_dict('records')
     elif value_top == 'past_occ':
         data_top = past_occ.to_dict('records')
-    elif value_top == 'past_adr':
-        data_top = past_adr.to_dict('records')
-    elif value_top == 'future_adr':
-        data_top = future_adr.to_dict('records')
-    elif value_top == 'past_revpab':
-        data_top = past_revpab.to_dict('records')
-    elif value_top == 'future_revpab':
-        data_top = future_revpab.to_dict('records')
-    
+    else:
+        data_top = []
+
     if value_middle == 'future_rev':
         data_middle = future_rev.to_dict('records')
     elif value_middle == 'future_occ':
         data_middle = future_occ.to_dict('records')
     elif value_middle == 'past_rev':
         data_middle = past_rev.to_dict('records')
-    elif value_middle == 'past_occ':
+    elif value_middle== 'past_occ':
         data_middle = past_occ.to_dict('records')
-    elif value_middle == 'past_adr':
-        data_middle = past_adr.to_dict('records')
-    elif value_middle == 'future_adr':
-        data_middle = future_adr.to_dict('records')
-    elif value_middle == 'past_revpab':
-        data_middle = past_revpab.to_dict('records')
-    elif value_middle == 'future_revpab':
-        data_middle = future_revpab.to_dict('records')
+    else:
+        data_middle = []
 
     if value_bottom == 'future_rev':
         data_bottom = future_rev.to_dict('records')
@@ -367,16 +275,14 @@ def update_data_table(value_top, value_middle, value_bottom):
         data_bottom = past_rev.to_dict('records')
     elif value_bottom == 'past_occ':
         data_bottom = past_occ.to_dict('records')
-    elif value_bottom == 'past_adr':
-        data_bottom = past_adr.to_dict('records')
-    elif value_bottom == 'future_adr':
-        data_bottom = future_adr.to_dict('records')
-    elif value_bottom == 'past_revpab':
-        data_bottom = past_revpab.to_dict('records')
-    elif value_bottom == 'future_revpab':
-        data_bottom = future_revpab.to_dict('records')
+    else:
+        data_bottom = []
 
     return data_top, data_middle, data_bottom
 
+# Run the App
+server = app.server
+
 if __name__ == '__main__':
     app.run_server(debug=True)
+
