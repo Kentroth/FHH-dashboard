247689dfe0250ce620f379df812115280b38c981
Kentroth <kentroth+1@gmail.com>
2024-02-21 09:06:56 -0600

    added organic booking calculations

--- Output/fhh_dash.py
+++ Output/fhh_dash.py
@@ -12,6 +12,8 @@ future_adr = pd.read_excel('future_adr.xlsx')
 past_adr = pd.read_excel('past_adr.xlsx')
 future_revpab = pd.read_excel('future_revpab.xlsx')
 past_revpab = pd.read_excel('past_revpab.xlsx')
+future_book = pd.read_excel('future_book.xlsx')
+past_book = pd.read_excel('past_book.xlsx')
 
 # Initialize the app - incorporate a Dash Mantine theme
 external_stylesheets = [dmc.theme.DEFAULT_COLORS]
@@ -22,6 +24,7 @@ style_rev = []
 style_occ = []
 style_adr = []
 style_revpab = []
+style_book = []
 
 for col in future_rev.columns[2:33]:
     style_rev.append(
@@ -310,6 +313,77 @@ for col in future_rev.columns[2:33]:
             'color': 'white'
         }
     )
+for col in future_rev.columns[2:33]:
+    style_book.append(
+        {
+            'if': {
+                'column_id': str(col),
+                'filter_query': '{{{}}} = 0'.format(col)
+            },
+            'backgroundColor': 'white',
+            'color': 'white'
+        }
+    )
+    style_book.append(
+        {
+            'if': {
+                'column_id': str(col),
+                'filter_query': '{{{}}} >= .01 && {{{}}} <= .2'.format(col, col)
+            },
+            'backgroundColor': '#ea5545',
+            'color': 'white'
+        }
+    )
+    style_book.append(
+        {
+            'if': {
+                'column_id': str(col),
+                'filter_query': '{{{}}} >= .21 && {{{}}} <= .4'.format(col, col)
+            },
+            'backgroundColor': '#ef9b20',
+            'color': 'white'
+        }
+    )
+    style_book.append(
+        {
+            'if': {
+                'column_id': str(col),
+                'filter_query': '{{{}}} >= .41 && {{{}}} <= .6'.format(col, col)
+                    },
+            'backgroundColor': '#e3d219',
+            'color': 'white'
+        }
+    )
+    style_book.append(
+        {
+            'if': {
+                'column_id': str(col),
+                'filter_query': '{{{}}} >= .61 && {{{}}} <= .8'.format(col, col)
+                    },
+            'backgroundColor': '#bdcf32',
+            'color': 'white'
+        }
+    )
+    style_book.append(
+        {
+            'if': {
+                'column_id': str(col),
+                'filter_query': '{{{}}} >= .8 && {{{}}} <= .82'.format(col, col)
+                    },
+            'backgroundColor': '#87bc45',
+            'color': 'white'
+        }
+    )
+    style_book.append(
+        {
+            'if': {
+                'column_id': str(col),
+                'filter_query': '{{{}}} >= .86'.format(col)
+                    },
+            'backgroundColor': '#364b1b',
+            'color': 'white'
+        }
+    )
 
 
 # Round the datasets to desired decimal places
@@ -321,6 +395,8 @@ future_adr = future_adr.round(0)
 past_adr = past_adr.round(0)
 future_revpab = future_revpab.round(0)
 past_revpab = past_revpab.round(0)
+future_book = future_book.round(2)
+past_book = past_book.round(2)
 
 
 # App layout
@@ -341,6 +417,8 @@ app.layout = dmc.Container(
                                 dmc.Radio('Past ADR', value='past_adr'),
                                 dmc.Radio('Future RevPAB', value='future_revpab'),
                                 dmc.Radio('Past RevPAB', value='past_revpab'),
+                                dmc.Radio('Future Organic %', value='future_book'),
+                                dmc.Radio('Past Organic %', value='past_book'),
 
                             ],
                             id='my-dmc-radio-item-top',
@@ -373,6 +451,8 @@ app.layout = dmc.Container(
                                 dmc.Radio('Past ADR', value='past_adr'),
                                 dmc.Radio('Future RevPAB', value='future_revpab'),
                                 dmc.Radio('Past RevPAB', value='past_revpab'),
+                                dmc.Radio('Future Organic %', value='future_book'),
+                                dmc.Radio('Past Organic %', value='past_book'),
 
                             ],
                             id='my-dmc-radio-item-mid',
@@ -434,6 +514,12 @@ def update_data_table(value_top, value_middle):
     elif value_top == 'past_revpab':
         data_top = past_revpab.to_dict('records')
         style_conditional = style_revpab
+    elif value_top == 'future_book':
+        data_top = future_revpab.to_dict('records')
+        style_conditional = style_book
+    elif value_top == 'past_book':
+        data_top = past_revpab.to_dict('records')
+        style_conditional = style_book
     
     if value_middle == 'future_rev':
         data_mid = future_rev.to_dict('records')
@@ -459,8 +545,14 @@ def update_data_table(value_top, value_middle):
     elif value_middle == 'past_revpab':
         data_mid = past_revpab.to_dict('records')
         style_conditional_1 = style_revpab
+    elif value_middle == 'future_book':
+        data_mid = future_revpab.to_dict('records')
+        style_conditional_1 = style_book
+    elif value_middle == 'past_book':
+        data_mid = past_revpab.to_dict('records')
+        style_conditional_1 = style_book
 
-    return data_top, style_conditional, data_mid, style_conditional_1  # Make sure to define style_conditional_mid
+    return data_top, style_conditional, data_mid, style_conditional_1  
 
 # Run the App
 server = app.server
